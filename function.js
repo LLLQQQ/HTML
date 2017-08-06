9c5cad54930102a6b11cba18f830b41db479b928
LLLQQQ <1170494354@qq.com>
2017-08-06 23:11:15 +0800

    any

---
+++ function.js
@@ -0,0 +1,20 @@
+function popUp(winURL) {
+    window.open(winURL, "popup", "width=320,height=480");
+}
+
+function prepareLinks() {
+    if (!document.getElementById) {
+        return false;
+    }
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].getAttribute("class") == "popup") {
+            links[i].onclick = function () {
+                popUp(this.getAttribute("href"));
+                return false;
+            }
+        }
+    }
+}
+
+window.onload = prepareLinks();
